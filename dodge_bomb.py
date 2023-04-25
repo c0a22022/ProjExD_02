cc668b3492a74cf81eebbbf7ac2cac29c6265a5c
Tomoki Ushiyama <c0a22022ae@edu.teu.ac.jp>
2023-04-25 15:54:44 +0900

    練習6

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -71,8 +71,8 @@ def main():
         if not tate:
             vy *= -1
         screen.blit(bb_img,bb_rct) #3
-        #if kk_rct.collidedict(bb_rct):
-         #   return
+        if kk_rct.colliderect(bb_rct):  #６
+            return
 
         pg.display.update()
         clock.tick(1000)
