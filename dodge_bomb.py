9ec0038a328eb5d553fa289e73a6934bde0a3655
Tomoki Ushiyama <c0a22022ae@edu.teu.ac.jp>
2023-04-25 14:13:22 +0900

    練習１

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -3,12 +3,16 @@ import sys
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
-    screen = pg.display.set_mode((1600, 500))
+    screen = pg.display.set_mode((1600, 600))
     clock = pg.time.Clock()
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    bb_img = pg.Surface((20,20))
+    pg.draw.circle(bb_img,(255,0,0),(10,10),10)  # 爆弾
+    bb_img.set_colorkey((0,0,0))  #爆弾よすみ
     tmr = 0
+    
 
     while True:
         for event in pg.event.get():
@@ -18,6 +22,7 @@ def main():
         tmr += 1
         screen.blit(bg_img, [0, 0])
         screen.blit(kk_img, [900, 400])
+        screen.blit(bb_img, [600, 200])
 
         pg.display.update()
         clock.tick(1000)
